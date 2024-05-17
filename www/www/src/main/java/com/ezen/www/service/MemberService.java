250d7a386cc051245dca6f82d8c8a118f0e0cc3c
dhalfks <miran5093@gmail.com>
2024-05-17 09:15:31 +0900

    security

---
+++ www/www/src/main/java/com/ezen/www/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.ezen.www.service;
+
+import com.ezen.www.domain.MemberVO;
+
+import java.util.List;
+
+public interface MemberService {
+  int insert(MemberVO mvo);
+
+  List<MemberVO> getList();
+}
